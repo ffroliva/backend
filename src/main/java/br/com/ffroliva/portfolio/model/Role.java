d40f47bcdcd5651c1a25f8cd18dec0400c9d2f57
Flavio <ffroliva@gmail.com>
2019-06-28 22:54:24 -0300

    Changing Role @Table name.

--- src/main/java/br/com/ffroliva/portfolio/model/Role.java
+++ src/main/java/br/com/ffroliva/portfolio/model/Role.java
@@ -4,15 +4,7 @@ package br.com.ffroliva.portfolio.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
@@ -28,6 +20,11 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Getter
 @Entity
+@Table(name = "roles", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+                "name"
+        })
+})
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
